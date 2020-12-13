c42a952ee25d693f700db57a2e790f078eb14747
Anna <annaandreevna90394@gmail.com>
2020-12-13 18:46:48 +0300

    add slider layout

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+
+let cssFiles = [
+	'./src/precss/reset.css',
+	'./src/precss/header.css',
+	'./src/precss/slider.css'
+];
+
+function styles(){
+	return gulp.src(cssFiles)
+			   .pipe(concat('main.css'))
+			   .pipe(autoprefixer({
+		            // browsers: ['>0.1%'],
+		            // cascade: false
+		        }))
+			   .pipe(cleanCSS({
+			   		level: 2
+			   	}))
+			   .pipe(gulp.dest('./src/css'))
+			   .pipe(browserSync.stream());
+
+};
+
+gulp.task('styles', styles); 
+
+gulp.task('watch', function(){
+	browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+
+	gulp.watch('./src/precss/**/*.css', styles);
+});
